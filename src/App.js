e6df2322921932edac97809362649100ab90407a
Shabanaasgar1997 <shabana.webdevloper@gmail.com>
2025-05-06 16:18:58 +0530

    added a new component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./Components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1 style={{ color: "red" }}>HEY WELCOME TO THE DEPLOYMENT PROJECT</h1>
-    </div>
+    <>
+      <div className="App">
+        <h1 style={{ color: "red" }}>HEY WELCOME TO THE DEPLOYMENT PROJECT</h1>
+      </div>
+      <Home />
+    </>
   );
 }
 
